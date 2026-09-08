ee6056c729cb19e65e7062439e937ca21f28b96f
Niccher <domi777nicch@gmail.com>
2026-09-08 17:26:57 +0300

    feat(ui): align Android app theme colors with Ace Admin palette

--- app/src/main/java/com/niccher/mpesa_analyzer_app/ui/theme/Color.kt
+++ app/src/main/java/com/niccher/mpesa_analyzer_app/ui/theme/Color.kt
@@ -2,10 +2,23 @@ package com.niccher.mpesa_analyzer_app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Ace Admin Brand Palette
+val AcePrimary = Color(0xFF438EB9)
+val AcePrimaryDark = Color(0xFF222A2D)
+val AceSecondary = Color(0xFF2B7DBC)
+
+val AceSuccess = Color(0xFF87B87F)
+val AceDanger = Color(0xFFD15B47)
+val AceWarning = Color(0xFFFFB752)
+
+val AceBgLight = Color(0xFFF2F5F8)
+val AceCardLight = Color(0xFFFFFFFF)
+
+// Legacy Material Compose Palette
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF438EB9)
+val PurpleGrey40 = Color(0xFF222A2D)
+val Pink40 = Color(0xFF2B7DBC)
